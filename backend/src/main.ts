0a00ebdf31338dd3fddcc0ad17337f856f5d81c0
Liam <jlhs1001@gmail.com>
2022-05-04 09:54:34 -0400

    add Index.ts to global package

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,28 +1,66 @@
+import fs from 'fs';
+import path from 'path';
+
+export function debug(message: string) {
+  basicLog(message, 'WARNING');
+}
+
+export function info(message: string) {
+  basicLog(message, 'WARNING');
+}
+
+export function warning(message: string) {
+  basicLog(message, 'WARNING');
+}
+
+export function error(message: string) {
+  basicLog(message, 'WARNING');
+}
+
+export function critical(message: string) {
+  basicLog(message, 'WARNING');
+}
+
+function basicLog(message: string, loglevel: string) {
+  const logfile = path.join(
+    __dirname,
+    'logs',
+    `${new Date().getUTCFullYear()}`,
+    `${new Date().getUTCMonth()}`,
+    `${new Date().getUTCDay()}`,
+  );
+
+  console.log(logfile);
+
+  const stream = fs.createWriteStream(logfile, { flags: 'a' });
+  stream.write(`${new Date().toISOString()}:${loglevel}:${message}`);
+  stream.end();
+}
+
 import {
-  origin,
-  port,
-  ClientToServerEvents,
-  ServerToClientEvents,
-  InterServerEvents,
-  SocketData,
-} from 'global';
+
+  // ClientToServerEvents,
+  // ServerToClientEvents,
+  // InterServerEvents,
+  // SocketData,
+} from 'hjb-global';
 
 import {
   createServer,
 } from 'http';
 
 import express from 'express';
-import { Server } from 'socket.io';
+// import { Server } from 'socket.io';
 
 const server = createServer();
-const io = new Server<ClientToServerEvents, ServerToClientEvents,
-  InterServerEvents, SocketData>(server, {
-  cors: {
-    origin: origin,
-    allowedHeaders: ['opcode'],
-    credentials: true,
-  },
-});
+// const io = new Server<ClientToServerEvents, ServerToClientEvents,
+//   InterServerEvents, SocketData>(server, {
+//   cors: {
+//     origin: origin,
+//     allowedHeaders: ['opcode'],
+//     credentials: true,
+//   },
+// });
 
 
 
@@ -30,6 +68,8 @@ const app = express(server);
 
 app.get('/', (_, res) => {
   res.send('hello world');
+  console.log("hola");
+  info("hello world!");
 });
 
 app.listen(port, () => {
